0f4348914533dcd41a1e6fb0a198500486272765
yuval <yuval.omer2@mail.huji.ac.il>
2023-09-14 19:52:05 +0300

    created initial networks and training procedure

--- Random_Terran_Bot.py
+++ Random_Terran_Bot.py
@@ -27,38 +27,41 @@ class RandomTerranBot(BotAI):
 
             vec, game_map = self.features_extractor.generate_vectors(iteration, iteration)
 
+            print(game_map.shape)
+            print(vec.shape)
+
             cv2.imshow('map', cv2.flip(cv2.resize(game_map, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST), 0))
             cv2.waitKey(1)
 
-            print(f"minerals: {vec[0]}")
-            print(f"vespene: {vec[1]}")
-            print(f"iteration: {vec[2]}")
-            print(f"supply used: {vec[3]}")
-            print(f"supply cap: {vec[4]}")
-            print(f"is_stimmed: {vec[5]}")
-            print(f"has_energy: {vec[6]}")
-            print(f"TERRANINFANTRYWEAPONSLEVEL1: {vec[7]}")
-            print(f"TERRANINFANTRYWEAPONSLEVEL2: {vec[8]}")
-            print(f"TERRANINFANTRYWEAPONSLEVEL3: {vec[9]}")
-            print(f"TERRANINFANTRYARMORSLEVEL1: {vec[10]}")
-            print(f"TERRANINFANTRYARMORSLEVEL2: {vec[11]}")
-            print(f"TERRANINFANTRYARMORSLEVEL3: {vec[12]}")
-            print(f"STIMPACK: {vec[13]}")
-            print(f"SHIELDWALL: {vec[14]}")
-            print(f"action_memory: {vec[15: 35]}")
-            print(f"ally units:")
-            for i, uid in enumerate(GB.terran_unit_list):
-                print(f"{uid}: {vec[35 + i]}")
-            print(f"ally structures:")
-            for i, uid in enumerate(GB.terran_structures_list):
-                print(f"{uid}: {vec[41 + i]}")
-            print(f"enemy units:")
-            for i, uid in enumerate(GB.protoss_units_list):
-                print(f"{uid}: {vec[50 + i]}")
-            print(f"enemy structures:")
-            for i, uid in enumerate(GB.protoss_structures_list):
-                print(f"{uid}: {vec[69 + i]}")
-            print(f"locations and units: {vec[86: -1]}")
+            # print(f"minerals: {vec[0]}")
+            # print(f"vespene: {vec[1]}")
+            # print(f"iteration: {vec[2]}")
+            # print(f"supply used: {vec[3]}")
+            # print(f"supply cap: {vec[4]}")
+            # print(f"is_stimmed: {vec[5]}")
+            # print(f"has_energy: {vec[6]}")
+            # print(f"TERRANINFANTRYWEAPONSLEVEL1: {vec[7]}")
+            # print(f"TERRANINFANTRYWEAPONSLEVEL2: {vec[8]}")
+            # print(f"TERRANINFANTRYWEAPONSLEVEL3: {vec[9]}")
+            # print(f"TERRANINFANTRYARMORSLEVEL1: {vec[10]}")
+            # print(f"TERRANINFANTRYARMORSLEVEL2: {vec[11]}")
+            # print(f"TERRANINFANTRYARMORSLEVEL3: {vec[12]}")
+            # print(f"STIMPACK: {vec[13]}")
+            # print(f"SHIELDWALL: {vec[14]}")
+            # print(f"action_memory: {vec[15: 35]}")
+            # print(f"ally units:")
+            # for i, uid in enumerate(GB.terran_unit_list):
+            #     print(f"{uid}: {vec[35 + i]}")
+            # print(f"ally structures:")
+            # for i, uid in enumerate(GB.terran_structures_list):
+            #     print(f"{uid}: {vec[41 + i]}")
+            # print(f"enemy units:")
+            # for i, uid in enumerate(GB.protoss_units_list):
+            #     print(f"{uid}: {vec[50 + i]}")
+            # print(f"enemy structures:")
+            # for i, uid in enumerate(GB.protoss_structures_list):
+            #     print(f"{uid}: {vec[69 + i]}")
+            # print(f"locations and units: {vec[86: -1]}")
 
         await self.distribute_workers()
 
---
+++ pytorch_agent.py
@@ -0,0 +1,208 @@
+from collections import defaultdict
+
+import torch
+from tensordict.nn import TensorDictModule
+from tensordict.nn.distributions import NormalParamExtractor
+from torch import nn
+from torchrl.collectors.collectors import SyncDataCollector
+from torchrl.data.replay_buffers import ReplayBuffer
+from torchrl.data.replay_buffers.samplers import SamplerWithoutReplacement
+from torchrl.data.replay_buffers.storages import LazyTensorStorage
+from torchrl.envs import (Compose, DoubleToFloat, ObservationNorm,
+                          TransformedEnv)
+from env import Sc2Env
+from torchrl.envs.libs.gym import GymWrapper
+import torch.nn.functional as F
+from torchrl.envs.utils import set_exploration_mode
+from torchrl.modules import ProbabilisticActor, TanhNormal, ValueOperator
+from torchrl.objectives import ClipPPOLoss
+from torchrl.objectives.value import GAE
+from tqdm import tqdm
+
+device = "cpu" if not torch.has_cuda else "cuda:0"
+num_cells = 256  # number of cells in each layer i.e. output dim.
+lr = 3e-4
+max_grad_norm = 1.0
+
+frame_skip = 1
+frames_per_batch = 100 // frame_skip
+total_frames = 1_000_000 // frame_skip
+
+sub_batch_size = 16  # cardinality of the sub-samples gathered from the current data in the inner loop
+num_epochs = 10  # optimization steps per batch of data collected
+clip_epsilon = (
+    0.2  # clip value for PPO loss: see the equation in the intro for more context.
+)
+gamma = 0.99
+lmbda = 0.95
+entropy_eps = 1e-4
+
+env = GymWrapper(Sc2Env())
+
+env = TransformedEnv(
+    env,
+    Compose(
+        # normalize observations
+        ObservationNorm(in_keys=["observation"]),
+        DoubleToFloat(in_keys=["observation"])
+    ),
+)
+
+env.transform[0].init_stats(num_iter=1000, reduce_dim=0, cat_dim=0)
+
+
+class Actor(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 5, 3)
+        self.conv2 = nn.Conv2d(5, 10, 3)
+        self.conv3 = nn.Conv2d(10, 4, 3)
+        self.fc_image = nn.LazyLinear(100)
+        self.fc1 = nn.LazyLinear(num_cells)
+        self.fc2 = nn.LazyLinear(num_cells)
+        self.fc3 = nn.LazyLinear(2 * env.action_spec.shape[-1])
+        self.pool = nn.MaxPool2d(2)
+        self.param_ex = NormalParamExtractor()
+
+    def forward(self, inp):  # todo: will definitely have some dimension problems
+        observation, game_map = inp[:, :87], inp[:, 88]
+        game_map = F.relu(self.conv1(game_map))
+        game_map = self.pool(game_map)
+        game_map = F.relu(self.conv2(game_map))
+        game_map = self.pool(game_map)
+        game_map = F.relu(self.conv3(game_map))
+        game_map = self.pool(game_map)
+        game_map = torch.flatten(game_map, 1)  # flatten all dims except batch dim
+        game_map = F.relu(self.fc_image(game_map))
+        X = torch.cat((observation, game_map), 1)
+        X = F.tanh(self.fc1(X))
+        X = F.tanh(self.fc2(X))
+        X = F.tanh(self.fc3(X))
+        return self.param_ex(X)
+
+
+actor_net = Actor()
+actor_net.to(device)
+policy_module = TensorDictModule(
+    actor_net, in_keys=["observation"], out_keys=["loc", "scale"]
+)
+
+policy_module = ProbabilisticActor(
+    module=policy_module,
+    spec=env.action_spec,
+    in_keys=["loc", "scale"],
+    distribution_class=TanhNormal,
+    return_log_prob=True,
+)
+
+value_net = Actor()
+value_net.to(device)
+value_module = ValueOperator(
+    module=value_net,
+    in_keys=["observation"],
+)
+
+collector = SyncDataCollector(
+    env,
+    policy_module,
+    frames_per_batch=frames_per_batch,
+    total_frames=total_frames,
+    split_trajs=False,
+    device=device,
+)
+
+replay_buffer = ReplayBuffer(
+    storage=LazyTensorStorage(frames_per_batch),
+    sampler=SamplerWithoutReplacement(),
+)
+
+advantage_module = GAE(
+    gamma=gamma, lmbda=lmbda, value_network=value_module, average_gae=True
+)
+
+loss_module = ClipPPOLoss(
+    actor=policy_module,
+    critic=value_module,
+    advantage_key="advantage",
+    clip_epsilon=clip_epsilon,
+    entropy_bonus=bool(entropy_eps),
+    entropy_coef=entropy_eps,
+    # these keys match by default but we set this for completeness
+    value_target_key=advantage_module.value_target_key,
+    critic_coef=1.0,
+    gamma=0.99,
+    loss_critic_type="smooth_l1",
+)
+
+optim = torch.optim.Adam(loss_module.parameters(), lr)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+    optim, total_frames // frames_per_batch, 0.0
+)
+
+logs = defaultdict(list)
+pbar = tqdm(total=total_frames * frame_skip)
+eval_str = ""
+
+# We iterate over the collector until it reaches the total number of frames it was
+# designed to collect:
+for i, tensordict_data in enumerate(collector):
+    # we now have a batch of data to work with. Let's learn something from it.
+    for _ in range(num_epochs):
+        # We'll need an "advantage" signal to make PPO work.
+        # We re-compute it at each epoch as its value depends on the value
+        # network which is updated in the inner loop.
+        advantage_module(tensordict_data)
+        data_view = tensordict_data.reshape(-1)
+        replay_buffer.extend(data_view.cpu())
+        for _ in range(frames_per_batch // sub_batch_size):
+            subdata = replay_buffer.sample(sub_batch_size)
+            loss_vals = loss_module(subdata.to(device))
+            loss_value = (
+                loss_vals["loss_objective"]
+                + loss_vals["loss_critic"]
+                + loss_vals["loss_entropy"]
+            )
+
+            # Optimization: backward, grad clipping and optimization step
+            loss_value.backward()
+            # this is not strictly mandatory but it's good practice to keep
+            # your gradient norm bounded
+            torch.nn.utils.clip_grad_norm_(loss_module.parameters(), max_grad_norm)
+            optim.step()
+            optim.zero_grad()
+
+    logs["reward"].append(tensordict_data["next", "reward"].mean().item())
+    pbar.update(tensordict_data.numel() * frame_skip)
+    cum_reward_str = (
+        f"average reward={logs['reward'][-1]: 4.4f} (init={logs['reward'][0]: 4.4f})"
+    )
+    logs["step_count"].append(tensordict_data["step_count"].max().item())
+    stepcount_str = f"step count (max): {logs['step_count'][-1]}"
+    logs["lr"].append(optim.param_groups[0]["lr"])
+    lr_str = f"lr policy: {logs['lr'][-1]: 4.4f}"
+    if i % 10 == 0:
+        # We evaluate the policy once every 10 batches of data.
+        # Evaluation is rather simple: execute the policy without exploration
+        # (take the expected value of the action distribution) for a given
+        # number of steps (1000, which is our ``env`` horizon).
+        # The ``rollout`` method of the ``env`` can take a policy as argument:
+        # it will then execute this policy at each step.
+        with set_exploration_mode("mean"), torch.no_grad():
+            # execute a rollout with the trained policy
+            eval_rollout = env.rollout(1000, policy_module)
+            logs["eval reward"].append(eval_rollout["next", "reward"].mean().item())
+            logs["eval reward (sum)"].append(
+                eval_rollout["next", "reward"].sum().item()
+            )
+            logs["eval step_count"].append(eval_rollout["step_count"].max().item())
+            eval_str = (
+                f"eval cumulative reward: {logs['eval reward (sum)'][-1]: 4.4f} "
+                f"(init: {logs['eval reward (sum)'][0]: 4.4f}), "
+                f"eval step-count: {logs['eval step_count'][-1]}"
+            )
+            del eval_rollout
+    pbar.set_description(", ".join([eval_str, cum_reward_str, stepcount_str, lr_str]))
+
+    # We're also using a learning rate scheduler. Like the gradient clipping,
+    # this is a nice-to-have but nothing necessary for PPO to work.
+    scheduler.step()
